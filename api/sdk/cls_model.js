a44f5fefab0c909a3c3a9597470c2a3b3fc1827c
anzala0807 <84629684+anzala0807@users.noreply.github.com>
2021-06-03 16:57:36 +0700

    Update cls_model.js

--- api/sdk/cls_model.js
+++ api/sdk/cls_model.js
@@ -31,7 +31,7 @@ async function classify(data){
 
     try{
         // path load in public access => github
-        const path = 'https://raw.githubusercontent.com/anzala0807/jst-week-12-/main/public/cls_model/model.json';
+        const path = 'https://raw.githubusercontent.com/anzala0807/newweek12/main/public/cls_model/model.json';
         const model = await tf.loadGraphModel(path);
         
         predict = model.predict(
